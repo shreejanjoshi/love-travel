2a9862f2b8b241b6f6eba10b301675b0aa406259
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-17 10:28:07 +0100

    made the style css for nav

--- src/components/layout/MainNav.js
+++ src/components/layout/MainNav.js
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 
+// classes name can you choose
+import classes from './MainNav.module.css';
+
 function MainNav(){
     return(
-        <header>
-            <div>Love Travel</div>
+        <header className={classes.header}>
+            <div className={classes.logo}>Love Travel</div>
             <nav>
                 <ul>
                     <li><Link to='/'>All Places</Link></li>
-                    <li><Link to='/newPlace'>All Places</Link></li>
+                    <li><Link to='/newPlace'>New Place</Link></li>
                     <li><Link to='/visited'>Visited</Link></li>
                 </ul>
             </nav>
