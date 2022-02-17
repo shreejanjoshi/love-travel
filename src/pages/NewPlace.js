62e99d3df885af2fa68ad76347ed5a476d7e4c00
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-17 15:40:02 +0100

    made account in firebase and send a post request to fairbase realtime database

--- src/pages/NewPlace.js
+++ src/pages/NewPlace.js
@@ -2,7 +2,21 @@ import NewTravelPlaceForm from "../components/travelPlace/NewTravelPlaceForm";
 
 function NewPlacePage(){
     function addTravelPlaceHandler(travelPlaceData){
-        
+        //sending http request we can use fetch function
+        //fetch is get request we need post request
+        fetch(
+            'https://lovel-travel-default-rtdb.europe-west1.firebasedatabase.app/travelplace.json', 
+            {
+                method: 'POST',
+                //we also add data to request data which we wanna store
+                //create json
+                body: JSON.stringify(travelPlaceData),
+                headers: {
+                    //extra meta data to make it more clear this is request carry json data
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
     }
 
     return(
