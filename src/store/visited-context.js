83d90ed23974d0a806d6f47f84d6367960cd591f
Shreejan Joshi <shreejanjoshi97@gmail.com>
2022-02-17 17:22:55 +0100

    context logic and trying different ways of updating state

--- src/store/visited-context.js
+++ src/store/visited-context.js
@@ -10,6 +10,28 @@ const VistiedContext = createContext({
 function VistiedContextProvider(props){
 
     const [userVisted, setUserVisited] = useState([]);
+
+    function addVisitedHandler(visitedTavelPlace){
+        //concat is like push but return new array with that added meetup
+        //get latest snapshot
+        setUserVisited((prevUserVisited) => {
+            return prevUserVisited.concat(visitedTavelPlace);
+        })
+    }
+
+    function removeVisitedHandler(travelPlaceId){
+        setUserVisited(prevUserVisited =>{
+            //get new array
+            //drop that item
+            return prevUserVisited.filter(travelPlace => travelPlace.id != travelPlaceId);
+        });
+    }
+
+    function itemIsVisitedHandler(travelPlaceId){
+        //some build in funcution
+        return userVisted.some(travelPlace => travelPlace.id === travelPlaceId);
+    }
+
     const context = {
         favorites: userVisted,
         totalVisited: userVisted.length,
